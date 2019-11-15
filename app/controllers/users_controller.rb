3da1e4a708f847479c150c8cdc11056de75e89dd
isyt729 <nwb0807@icloud.com>
2019-11-15 14:36:36 +0900

    Revert "kami"
    This reverts commit ccda4f3f303a50d59d3b9e1c019465b5421f111e.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,4 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.search(params[:keyword], current_user.id)
-    respond_to do |format|
-      format.html
-      format.json
-    end
-  end
-
   def edit
   end
 
