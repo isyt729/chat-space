98b13b5a65730a8b58d0a7d82af9397e051dd748
isyt729 <nwb0807@icloud.com>
2019-11-20 16:36:46 +0900

    first

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -19,10 +19,35 @@ $(function(){
     return html;
   }
 
+  var reloadMessages = function(){
+      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+      last_message_id = $('.message').last().data("message-id");
+      var group_num = (location.href).match(/\/groups\/(\d+)\/messages/);
+      $.ajax({
+        url: `/groups/${group_num[1]}/api/messages`,
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        //追加するHTMLの入れ物を作る
+        var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        messages.forEach(function (message){
+          insertHTML = buildMessage(message); 
+          $('.messages').append(insertHTML);
+        })
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight})
+      })
+      .fail(function() {
+        console.log('error');
+      });
+  };
+
   $('#new_message').on('submit', function(e){
     e.preventDefault()
     var formData = new FormData(this);
-    var url = $(this).attr('action')
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
@@ -44,4 +69,6 @@ $(function(){
       $('.submit-btn').prop('disabled', false)
     })
   })
+
+  setInterval(reloadMessages, 7000)
 })
